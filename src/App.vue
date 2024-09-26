1d4a32dfaec1e6d1acb31f0c2443759991a94448
Halí V <halivelazquez@outlook.com>
2024-09-25 21:36:59 -0600

    feat: add game

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,121 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+import { winnerCoordinates } from '@/winnerCoordinates'
+
+const X = 'X'
+const O = 'O'
+
+const board = ref([
+	['', '', ''],
+	['', '', ''],
+	['', '', '']
+])
+
+const turns = computed(() =>
+	board.value.reduce(
+		(current, row) => current + row.reduce((current, cell) => current + (Boolean(cell) ? 1 : 0), 0),
+		0
+	)
+)
+
+const turn = computed(() => (turns.value % 2 ? O : X))
+
+const winningCoordinates = computed(() => {
+	if (turns.value < 5) return null
+
+	for (let winningCase = 0; winningCase < winnerCoordinates.length; winningCase++) {
+		const [a, b, c] = winnerCoordinates[winningCase]
+
+		const [a0, a1] = a
+		const [b0, b1] = b
+		const [c0, c1] = c
+
+		if (
+			board.value[a0][a1] === board.value[b0][b1] &&
+			board.value[b0][b1] === board.value[c0][c1] &&
+			Boolean(board.value[a0][a1])
+		) {
+			return winnerCoordinates[winningCase]
+		}
+	}
+
+	return null
+})
+
+const winner = computed(() => {
+	if (!winningCoordinates.value) return ''
+
+	const coordinates = winningCoordinates.value[0]
+
+	return board.value[coordinates[0]][coordinates[1]]
+})
+
+const draw = computed(() => !winner.value && turns.value === 9)
+
+function update(i: number, j: number, turn: string) {
+	if (winner.value) return
+
+	board.value[i][j] = turn
+}
+
+function restart() {
+	board.value = [
+		['', '', ''],
+		['', '', ''],
+		['', '', '']
+	]
+}
+
+function partOfWinningCoordinates(i: number, j: number): boolean {
+	if (!winningCoordinates.value) return false
+
+	return winningCoordinates.value.some(([a, b]) => a === i && b === j)
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<div class="h-dvh max-w-prose mx-auto py-3 grid">
+		<section class="place-self-center flex flex-col gap-3 relative">
+			<div
+				v-if="winner || draw"
+				class="absolute inset-3 bg-zinc-800/40 rounded grid place-items-center"
+			>
+				<div class="bg-white py-4 px-8 rounded-sm flex gap-4 flex-col">
+					<p v-if="winner" class="text-4xl text-center">
+						Ganador! <br />
+						<strong>
+							{{ winner }}
+						</strong>
+					</p>
+					<p v-else-if="draw" class="text-4xl text-center">Tablas</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+					<button class="px-2 py-1 bg-zinc-700 rounded text-white" @click="restart">
+						Reiniciar
+					</button>
+				</div>
+			</div>
 
-  <main>
-    <TheWelcome />
-  </main>
+			<div class="flex gap-3" v-for="(row, i) in board" :key="i">
+				<button
+					:class="[
+						'bg-zinc-200 text-[10dvh] aspect-square h-[20dvh] text-center',
+						'rounded-lg',
+						{ 'accent-red-500': cell === X },
+						{ 'accent-teal-500': cell === O },
+						{ 'bg-red-200': cell === X && !winner },
+						{ 'bg-teal-200': cell === O && !winner },
+						{ 'bg-red-600 text-white': cell === X && partOfWinningCoordinates(i, j) },
+						{ 'bg-teal-600 text-white': cell === O && partOfWinningCoordinates(i, j) },
+						{ 'focus:bg-red-50 hover:bg-red-50 accent-red-800': turn === X && !cell },
+						{ 'focus:bg-teal-50 hover:bg-teal-50 accent-teal-800': turn === O && !cell }
+					]"
+					v-for="(cell, j) in row"
+					:key="`${i},${j}`"
+					@click="update(i, j, turn)"
+				>
+					{{ cell }}
+				</button>
+			</div>
+		</section>
+	</div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
